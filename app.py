31f4aff7074e02c7ae3af3a02119e9d9c6619f5b
Chrys Redin <redin.dev.87@gmail.com>
2022-07-26 11:50:03 -0500

    update routes

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_restful import  Api
 # Importacion de Rutas 
-""" from routes.users import users_routes
+from routes.users import users_routes
 from routes.documentos import documentos_routes
- """
 
 from resources.apidocumentos import DocumentosLista, Documento
 
@@ -11,7 +10,7 @@ app = Flask(__name__)
 api = Api(app)
 
 # Registro de la ruta
-""" app.register_blueprint(users_routes) """
-""" app.register_blueprint(documentos_routes) """
+app.register_blueprint(users_routes)
+app.register_blueprint(documentos_routes)
 api.add_resource(DocumentosLista, '/documents-list')
 api.add_resource(Documento, '/document/<string:id_doc>')
