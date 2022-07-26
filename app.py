c97f6f1a6f304abfe5fa9d8459d85ca8f346cb0f
Chrys Redin <redin.dev.87@gmail.com>
2022-07-26 11:01:13 -0500

    Flask RESTful working

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_restful import  Api
 # Importacion de Rutas 
-from routes.users import users_routes
+""" from routes.users import users_routes
+from routes.documentos import documentos_routes
+ """
+
+from resources.apidocumentos import DocumentosLista, Documento
 
 app = Flask(__name__)
+api = Api(app)
 
 # Registro de la ruta
-app.register_blueprint(users_routes)
+""" app.register_blueprint(users_routes) """
+""" app.register_blueprint(documentos_routes) """
+api.add_resource(DocumentosLista, '/documents-list')
+api.add_resource(Documento, '/document/<string:id_doc>')
--- database.sql
+++ database.sql
@@ -1,9 +1,23 @@
 -- TABLA USUARIOS
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users (
-    id_user uuid DEFAULT uuid_generate_v4 (),
+    id_user uuid DEFAULT uuid_generate_v4(),
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE documents (
+    id_doc uuid DEFAULT uuid_generate_v4(),
+    folio_doc VARCHAR(255),
+    tip_doc VARCHAR(255),
+    fecha_doc VARCHAR(255),
+    asunto_doc VARCHAR(255),
+    dirigido_doc VARCHAR(255),
+    contenido_doc VARCHAR(255),
+    copia_doc VARCHAR(255),
+    slbr_doc VARCHAR(255),
+    id_firma VARCHAR(255),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
